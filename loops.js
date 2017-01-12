9fd2bd984d403b2f74e54eb1425ab788cee17609
John Cibula <johnjcibula@gmail.com>
2017-01-12 21:58:41 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,12 @@
+forLoop(array){
+for (let i = 0, i<25,i++) {
+  if (i === 1) {
+    array = "I am 1 strange loop";
+    return array;
+  }
+  else {
+  array = "I am ${i} strange loops";
+  return array;
+  }
+}
+}
