28d24238d4fd99f1d137b082aaea49e6966ece21
John Cibula <johnjcibula@gmail.com>
2017-01-13 22:12:34 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,12 +1,32 @@
-forLoop(array){
-for (let i = 0, i<25,i++) {
-  if (i === 1) {
-    array = "I am 1 strange loop";
+function forLoop(array) {
+    for (let i = 0;i<25;i++){
+        if (i===0) {
+            array.push(`I am one strange loop`);
+        }
+        else {
+          array.push(`I am ${i} strange loops`);
+        }
+    }
     return array;
-  }
-  else {
-  array = "I am ${i} strange loops";
-  return array;
-  }
 }
+/***TODO**: Create a function called `whileLoop` in `loops.js`. The function should take a number as an argument. Using a `while` loop, count down (using `console.log`) from the passed in number to 0. Then return the string `'done'`.*/
+function whileLoop(n) {
+  while(n>0){
+    console.log(n);
+    n--;
+  }
+  return `done`;
+}
+/***TODO**: Define a function called `doWhileLoop` in `loops.js`. The function should take an array as an argument. Use the `maybeTrue()` function (you can copy it from this README) as the condition, and remove elements from the array until the array is empty or until `maybeTrue()` returns `false`. (Your condition might look something like `array.length > 0 && maybeTrue()`.) Finally, return the array.*/
+function doWhileLoop(array) {
+  function maybeTrue(){
+    return Math.random >= .5;
+  }
+  do{
+    array.pop();
+  }
+  while (array.length>0 && maybeTrue()){
+    array.pop();
+  }
+  return array;
 }
